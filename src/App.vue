faf684b5d5ad96de7a3a4832d5bcaa49293cbb26
Anyon <zoujingli@qq.com>
2018-07-17 17:10:04 +0800

    [更新]Vue更新

--- src/App.vue
+++ src/App.vue
@@ -16,8 +16,8 @@
         <router-view></router-view>
 
         <!-- 底部导航条 开始 -->
-        <div style="height: 70px"></div>
-        <mu-bottom-nav :value.sync="shift" shift id="BottomNav" color="pink">
+        <div style="height:70px"></div>
+        <mu-bottom-nav shift :value.sync="NavVal" id="NavEle" color="pink">
             <mu-bottom-nav-item to="/" value="home" title="首 页" icon="home"></mu-bottom-nav-item>
             <mu-bottom-nav-item to="/order" value="order" title="订 单" icon="reorder"></mu-bottom-nav-item>
             <mu-bottom-nav-item to="/cart" value="cart" title="购物车" icon="books"></mu-bottom-nav-item>
@@ -29,13 +29,12 @@
 </template>
 
 <script>
-
     export default {
         name: 'App',
         data() {
             return {
+                NavVal: 'home',
                 title: 'ThinkMobile',
-                // shift: 'home',
             }
         }
     }
@@ -47,14 +46,14 @@
     }
 
     #app {
-        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        font-family: Avenir, Helvetica, Arial, sans-serif;
         -webkit-font-smoothing: antialiased;
         -moz-osx-font-smoothing: grayscale;
         text-align: center;
         color: #2c3e50;
     }
 
-    #BottomNav {
+    #NavEle {
         position: fixed;
         bottom: 0;
         width: 100%;
